2c0d5fd074b51856cee7720a09baa4315bbdd7b8
Serhii Tishchenko <yourhostel.ua@gmail.com>
2023-06-10 19:40:47 +0300

    mid = l+r >>> 1

--- src/main/java/BinarySearch.java
+++ src/main/java/BinarySearch.java
@@ -6,19 +6,20 @@ public class BinarySearch {
         // массив a должен быть отсортированным
         int l = 0;                   // l, r — левая и правая границы
         int r = a.length - 1;
-       while (l <= r){
-            int m = l + (r - l) / 2; //m — середина области поиска
-            if (key < a[m]) r = m - 1;
-            else if (key > a[m]) l = m + 1;
-            else return m; //Сужение области поиска
+        while (l <= r){
+            int m = l + r >>> 1; //m — середина области поиска
+            int mV = a[m];
+            if (key < mV) r = m - 1;
+            else if (key > mV) l = m + 1;
+            else  return m; //Сужение области поиска
         }
        return -1; //элемент не найден
     }
 
     public static void main(String[] args) {
-        int[] a = new Random().ints(30, 1, 15).toArray();
+        int[] a = new Random().ints(30, 1, 14).toArray();
         Arrays.sort(a);
         System.out.println(Arrays.toString(a));
-        System.out.println(binarySearch(6, a));
+        System.out.println(binarySearch(2, a));
     }
 }

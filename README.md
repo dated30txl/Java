28c926dca3593169c3dda333bf42e53eeba5cbb9
Serhii Tishchenko <yourhostel.ua@gmail.com>
2023-09-01 17:54:15 +0300

    added gradingStudents to hackerrank

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 
 - [Binary search](https://github.com/dated30txl/Java/blob/main/src/main/java/BinarySearch.java)
 - [Merge sort](https://github.com/dated30txl/Java/blob/main/src/main/java/MergeSort.java)
+
+- [ ] hackerrank
+
+- [Grading students](https://github.com/dated30txl/Java/blob/main/src/main/java/GradingStudents.java)
---
+++ src/main/java/GradingStudents.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class GradingStudents {
+    public static List<Integer> gradingStudents(List<Integer> grades) {
+        return grades.stream()
+                .map(grade -> {
+                    if (grade < 38) {
+                        return grade;
+                    }
+                    int nextMultipleOfFive = (grade + 4) / 5 * 5;
+                    if (nextMultipleOfFive - grade < 3) {
+                        return nextMultipleOfFive;
+                    } else {
+                        return grade;
+                    }
+                })
+                .collect(Collectors.toList());
+
+    }
+
+    public static void main(String[] args) {
+        List<Integer> grades = Arrays.asList(73, 67, 38, 33);
+        System.out.println(gradingStudents(grades));
+    }
+}
